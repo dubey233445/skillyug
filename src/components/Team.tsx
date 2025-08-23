5325d9d3048879878238864c0f0b043037c18914
Ashish Dubey <159797729+dubey233445@users.noreply.github.com>
2025-08-23 23:27:36 +0530

    Update Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -30,7 +30,7 @@ const Team = () => {
     {
       name: "Vibhanshu Mishra",
       role: "Chief Operating Officer",
-      bio: "blockchain, Android development And Responsible for overseeing daily operations, optimizing processes, and ensuring strategic goals are met across all departments.",
+      bio: "Blockchain, Android development And Responsible for overseeing daily operations, optimizing processes, and ensuring strategic goals are met across all departments.",
       image: "v1.jpg",
       social: {
         linkedin: "https://www.linkedin.com/in/vibhanshu-mishra-8a351b336/",
